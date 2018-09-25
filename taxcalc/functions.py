8dabc8bae6d24add74940fb72fc91740a02738f5
shikharpant <muski.shikhar@gmail.com>
2018-09-25 17:03:13 +0530

    addrebateA

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -94,3 +94,9 @@ def pit_liability(calc):
                               np.maximum(0., taxinc - tbrk2)) +
            rate4 * np.maximum(0., taxinc - tbrk3))
     calc.array('pitax', tax)
+
+    def rebate(SALARIES):
+        "compute the rebate under section 87A of Income Tax Act"
+        Salaries_after_rebate = SALARIES
+        if SALARIES <500000 :
+            Salaries_after_rebate = SALARIES - 2000
